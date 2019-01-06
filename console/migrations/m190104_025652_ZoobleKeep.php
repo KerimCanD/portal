2c1beff77df2bf99166a3be73dfdd32088bfa5e1
KerimCanD <kcandagtekin@gmail.com>
2019-01-06 20:33:22 +0300

    initial commit

---
+++ console/migrations/m190104_025652_ZoobleKeep.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190104_025652_ZoobleKeep
+ */
+class m190104_025652_ZoobleKeep extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190104_025652_ZoobleKeep cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190104_025652_ZoobleKeep cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
